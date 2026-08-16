ca2350459ff8a370c19a132d7c51569d160673b5
tpbaccam <tpbaccam@gmail.com>
2026-08-16 10:39:49 -0700

    Added JS

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ function refreshWeather(response) {
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="icon" />`;
+
+  getForecast{response.data.city};
 }
 
 function formatDate(date) {
@@ -52,6 +54,44 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon","Tue","Wed","Thu", "Fri", "Sat"];
+  
+  return days[date.get.Day()];
+
+}
+
+function getForcast(city) {
+  let apiKey ="f06ffeb6b437a2d43f8900ca0tf29cod";
+  let apiURL = 'https://api.shecodes.io/weather/v1/forecast?query={city}&key={apiKey}&units=metric';
+  axios(apiURL).then(displayForecast);
+}
+
+function displayForecast(response) {
+  let forecastHtml = "";
+  response.data.daily.forEach(function (day, index) {
+    if (index <5){
+      forecastHtml = 
+      forecastHtml+
+      `
+      <div class="weather-forecast-day">
+      <div class = "weather-forecast-date">${formatDate(day.time)}</div>
+      
+      <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+      <div class="weather-forecast-temperatures">
+      <div class+"weather-forecast-temperature">
+      <strong>${Math.round(day.temperature.maximum)}º</strong>
+      </div>
+      <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum,)}º</div>
+      </div>
+      </div>
+      `;
+    }
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
